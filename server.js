f5d6fb95d42e727966aedc8381b697f4e20b3bfc
nipaaa <akternipa941@gmail.com>
2024-10-30 14:41:40 +0600

    email template make responsive

--- server.js
+++ server.js
@@ -9,12 +9,21 @@ const depositeRoutes = require("./routes/deposite.routes");
 const { sendEmail } = require("./utils/auth");
 const { isAuth } = require("./utils/middleware");
 
+const logkit = require('logkitx');
+const logger = require('pino')({
+  level: process.env.LEVEL || 'info'
+}, process.stderr);
+// Register logkitx early so modules that call debug (e.g. mongoose/mquery)
+// don't trigger the "debug called before logkitx initialized" error.
+logkit(logger, {
+  levels: ['trace', 'error', 'fatal', 'info', 'warn', 'debug'],
+  format: 'logfmt'
+});
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-connectDB();
+// connectDB()
 
 app.use("/api/users", userRoutes);
 app.use("/api/pools", poolRoutes);
